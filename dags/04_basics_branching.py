493dd346f7910e69448de82942ff4f998e5f0172
주대성 <dae102100@naver.com>
2026-04-09 11:21:22 +0900

    Feat: 분기 DAG 완료

--- dags/04_basics_branching.py
+++ dags/04_basics_branching.py
@@ -5,41 +5,66 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.trigger_rule import TriggerRule 
 import logging
+import random
 
+
+def _branching(**kwargs):
+    '''
+        특정 조건에 따라 분기 처리해서 다음 수행 지정
+    '''
+    if random.choice([True,False]):
+        logging.info('task process 실행')
+        return "process" #이동하고 싶은 task
+    else :
+        logging.info('skip process 실행')
+        return "skip"  
+
+
+    pass
+
+
+def _process(**kwargs):
+    logging.info('특정 목적 실행')
+    pass
 
 with DAG(
-    dag_id      = "03_basic_macro_jinja",
-    description = "macro를 통해 context 접근, jinja를 통해 표현 ",
+    dag_id      = "04_basics_branching",
+    description = "분기 처리, 선택적 TASK 구동",
     default_args={
         'owner' : 'de2teammanger',
         'retries' : 1,
         'retry_delay' : timedelta(minutes=1)
     },
-    schedule_interval = '0 9 * * *',
+    schedule_interval = '@daily',
     start_date = datetime(2026,2,25),
     catchup = False,
-    tags= ['jinja','macro','context']
+    tags= ['branch','trigger_rule']
 ) as dag:
     # 3. task 정의
     task_start = EmptyOperator(
-            task_id="jinja_used_bash",
+            task_id="start",
             # jinja에서 값 출력 표현식 => {{변수|값|식|함수 등등}}
             bash_command= "echo 'DAG t1 task 수행시간 {{ds}},{{ds_nodash}}'",
     )
     task_branch = BranchPythonOperator(
-        task_id="jinja_macro_bash",
-        bash_command= "echo '일주일전 수행시감 계산 {{macros.ds_add(ds,-7)}}'",
+        task_id="branching",
+        python_callable= _branching,
     )
     task_process = PythonOperator(
-        task_id="jinja_used_python",
-        python_callable= _print
+        task_id="process",
+        python_callable= _process
     )
-    task_skip = EmptyOperator()
-    task_end = EmptyOperator()
+    task_skip = EmptyOperator(
+        task_id="skip",
+    )
+    task_end = EmptyOperator(
+        task_id="end",
+        # 최소 1개는 성공해야함
+        trigger_rule= TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
+    )
 
     # 4. 의존성 정의
     task_start >> task_branch
