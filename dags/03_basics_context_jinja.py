35ee5b4e83f54c6e5b3b01219c8cfdc73ba8ea8c
주대성 <dae102100@naver.com>
2026-04-09 10:40:40 +0900

    수정

--- dags/03_basics_context_jinja.py
+++ dags/03_basics_context_jinja.py
@@ -8,13 +8,41 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 import logging
 
+#3-1 콜백 함수 정의
+def _prin(**kwargs):
+    logging.info(f'ds 출력 {kwargs['ds'] }')
+    logging.info(f'nodash 출력 {kwargs['ds_nodash']}')
+
+    pass
 
 # 2. DAG 정의
-with DAG() as dag:
+with DAG(
+    dag_id      = "03_basic_macro_jinja",
+    description = "macro를 통해 context 접근, jinja를 통해 표현 ",
+    default_args={}
+        'owner' : 'de2teammanger',
+        'retries' : 1,
+        'retry_delay' : timedelta(minutes=1)
+    },
+    schedule_interval = '0 9 * * *',
+    start_date = datetime(2026,2,25),
+    catchup = False,
+    tags= ['jinja','macro','context']
+) as dag:
     # 3. task 정의
-    t1 = BashOperator()
-    t2 = BashOperator()
-    t3 = PythonOperator()
+    t1 = BashOperator(
+            task_id="jinja_used_bash",
+            # jinja에서 값 출력 표현식 => {{변수|값|식|함수 등등}}
+            bash_command= "echo 'DAG t1 task 수행시간 {{ds}},{{ds_nodash}}'",
+    )
+    t2 = BashOperator(
+        task_id="jinja_macro_bash",
+        bash_command= "echo '일주일전 수행시감 계산 {{macros.ds_add(ds,-7)}}'",
+    )
+    t3 = PythonOperator(
+        task_id="jinja_used_python",
+        python_callable=""
+    )
 
     # 4. 의존성 정의
     t1 >> t2 >> t3
